8cf1c651f5947f48aa669931181804950409c92d
sunfanyi <sunfanyi_jim@outlook.com>
2023-02-09 15:33:11 +0000

    Plot Cartesian workspace animation.

--- tools.py
+++ tools.py
@@ -12,9 +12,47 @@ from IPython.display import display
 # sin(theta)
 
 
-def plot_link(ax, P1, P2):
-    ax.plot3D([P1[0], P2[0]], [P1[1], P2[1]], [P1[2], P2[2]], 'b-')
-    ax.plot3D([P1[0]], [P1[1]], [P1[2]], 'bx')
+def plot_links(ax, links, show_coor=False):
+    for i in range(len(links)-1):
+        P1 = links[i][:3]
+        P2 = links[i+1][:3]
+        ax.plot([P1[0], P2[0]], [P1[1], P2[1]], [P1[2], P2[2]], 'b-')
+        ax.plot([P1[0]], [P1[1]], [P1[2]], 'bx')
+
+        if show_coor:
+            P1 = [round(i) for i in P1]
+            P2 = [round(i) for i in P2]
+            if P1 != P2:
+                ax.text(P1[0] - 10, P1[1], P1[2] + 3, str(P1))
+    if show_coor:
+        ax.text(P2[0] - 10, P2[1], P2[2] - 15, str(P2))
+
+
+def plot_links_moving(line1, line2, links):
+    link_data = [[], [], []]
+    joint_data = [[], [], []]
+    for i in range(len(links)-1):
+        P1 = links[i][:3]
+        P2 = links[i+1][:3]
+
+        # Link line
+        link_data[0].extend([P1[0], P2[0]])
+        link_data[1].extend([P1[1], P2[1]])
+        link_data[2].extend([P1[2], P2[2]])
+
+        # Joint point
+        joint_data[0].extend([P1[0]])
+        joint_data[1].extend([P1[1]])
+        joint_data[2].extend([P1[2]])
+
+    line1.set_xdata(link_data[0])
+    line1.set_ydata(link_data[1])
+    line1.set_3d_properties(link_data[2])
+
+    # Joint point
+    line2.set_xdata(joint_data[0])
+    line2.set_ydata(joint_data[1])
+    line2.set_3d_properties(joint_data[2])
 
 
 def joint_print(joints, alias):
